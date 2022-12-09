cc042fd28498c5575bddb572b489904b95204c02
Einstein Ayres <117763987+Ayrs9189@users.noreply.github.com>
2022-12-09 10:16:49 -0300

    Adição da Classe Email
    Alteração na classe cliente e adição da Classe Email

--- src/br/com/devsjava/bytebank/modelo/Cliente.kt
+++ src/br/com/devsjava/bytebank/modelo/Cliente.kt
@@ -1,12 +1,20 @@
-package br.com.devsjava.bytebank.modelo
+package br.com.alura.bytebank.modelo
 
 import br.com.devsjava.bytebank.modelo.Autenticavel
 import br.com.devsjava.bytebank.modelo.Endereco
 
 class Cliente(
     var nome: String,
-    val cpf: String,
+    var idade: Int,
+    var cpf: Int;
+    var cnpj: Int;
+    var nome-pai: String
+    var nome-mae: String
+    var sexo: Boolean           //= (0 = "Masculino" = true || false = "Feminino")
+    var email: Email = Email(),
+    var doisTelefones: Int
     var endereco: Endereco = Endereco(),
+    var
     private val senha: Int
 ) : Autenticavel {
 
